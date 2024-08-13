fd4caf78fc699ff7faaa516a21f730c389d9f13d
wwuselessww <sa24122@gmail.com>
2024-08-13 16:11:36 +0300

    Initial Commit

---
+++ mobileUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobileUp
+//
+//  Created by Alexander Kozharin on 13.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
