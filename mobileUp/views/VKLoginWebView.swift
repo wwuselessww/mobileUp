eed37ee194737696c4d80a990c81935dc8795c0d
wwuselessww <sa24122@gmail.com>
2024-08-13 20:17:28 +0300

    got oauth token from vk

--- mobileUp/views/VKLoginWebView.swift
+++ mobileUp/views/VKLoginWebView.swift
@@ -20,9 +20,39 @@ class VKLoginWebView: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appID = "51640152"
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "oauth.vk.com"
+        urlComponent.path = "/authorize"
         
-        let url = URL(string: "https://google.com")!
+        urlComponent.queryItems = [
+            URLQueryItem(name: "client_id", value: appID),
+            URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
+            URLQueryItem(name: "display", value: "mobile"),
+            URLQueryItem(name: "response_type", value: "token"),
+        ]
+
+        guard let url = urlComponent.url else {fatalError("no url")}
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url, url.path == "/blank.html" else {
+            decisionHandler(.allow)
+            return
+        }
+        var token = ""
+        if let params = url.fragment?.components(separatedBy: "&").map({$0.components(separatedBy: "=")}) {
+            for index in params.enumerated() {
+                if index.element.first == "access_token" {
+                    token = index.element.last ?? "no token"
+                }
+            }
+        } else {
+            token = "no token"
+        }
+            print(token)
+        decisionHandler(.cancel)
     }
 }
